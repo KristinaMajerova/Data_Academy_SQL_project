e73474ebe8e837cb77e7cd330c64070d7b80a70f
KristinaMajerova <122482745+KristinaMajerova@users.noreply.github.com>
2023-03-19 19:55:14 +0100

    Update DA_scrit_SQL_project.sql

--- DA_scrit_SQL_project.sql
+++ DA_scrit_SQL_project.sql
@@ -2,11 +2,10 @@
 		
 -- dokaz ze region_code = NULL - priemerná hodnota krajov: 
 	
-SELECT *, avg(value) 
+SELECT *, date_from, category_code, avg(value) 
 FROM czechia_price cp
-GROUP BY date_from , category_code 
+GROUP BY date_from, category_code 
 ORDER BY region_code;
-	
 
 CREATE OR REPLACE TABLE t_kristina_majerova_project_SQL_primary_final AS
 SELECT * 
@@ -21,57 +20,55 @@ from (
 		LEFT JOIN czechia_price_category cpc 
 			ON cpc.code = base1.category_code
 		ORDER BY base1.price_year
-		) potraviny
+		) groceries
 JOIN 
 	(SELECT base2.*, cpib.name AS industry_branch_name
 	FROM (
-		SELECT cp.payroll_year , cp.industry_branch_code , round(avg(cp.value),2) AS avg_payroll
+		SELECT cp.payroll_year, cp.industry_branch_code, round(avg(cp.value),2) AS avg_payroll
 			FROM czechia_payroll cp 
 				WHERE cp.calculation_code = 200 
 				AND cp.value_type_code = 5958 
 				AND cp.industry_branch_code IS NOT NULL 
-			GROUP BY cp.industry_branch_code , cp.payroll_year
+			GROUP BY cp.industry_branch_code, cp.payroll_year
 			) base2
 		LEFT JOIN czechia_payroll_industry_branch cpib 
 		ON base2.industry_branch_code = cpib.code
-		) mzdy
-ON potraviny.price_year = mzdy.payroll_year;
-
+		) payroll
+ON groceries.price_year = payroll.payroll_year;
 
 -- CREATE SECONDARY TABLE FINAL
 
-CREATE OR REPLACE TABLE t_kristina_majerova_project_SQL_secondary_final as
-SELECT country, `year`, GDP, population , 
+CREATE OR REPLACE TABLE t_kristina_majerova_project_SQL_secondary_final AS
+SELECT country, `year`, GDP, population, 
     round(GDP/population,2) AS GDP_per_capita, 
     round(lead(GDP/population,-1)OVER (ORDER BY `year`),2) AS GDP_per_capita_prew,
-    round(((GDP/population/lead(GDP/population,-1)OVER (ORDER BY `year`)-1)*100),2) as rast_gdp_per_capita
+    round(((GDP/population/lead(GDP/population,-1)OVER (ORDER BY `year`)-1)*100),2) AS growth_gdp_per_capita
     FROM economies e
     WHERE country = 'Czech Republic' AND YEAR >= 2005
-    ORDER BY `year` ;
+    ORDER BY `year`;
 
 
 -- OTAZKA 1
 -- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 
-
-SELECT payroll_year, industry_branch_name, rast_avg_payroll
+SELECT payroll_year, industry_branch_name, growth_avg_payroll
 FROM (
     SELECT *, 
         lag(avg_payroll,1) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year) AS avg_payroll_lag,
-        round(avg_payroll/lag(avg_payroll,1) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year)*100-100,2) AS rast_avg_payroll
+        round(avg_payroll/lag(avg_payroll,1) OVER (PARTITION BY industry_branch_name ORDER BY payroll_year)*100-100,2) AS growth_avg_payroll
     FROM (
         SELECT payroll_year, avg_payroll, industry_branch_name
         FROM t_kristina_majerova_project_sql_primary_final
-        GROUP BY industry_branch_name, price_year
         ) base
     ) base2
-WHERE rast_avg_payroll < 0
+WHERE growth_avg_payroll < 0
 ORDER BY industry_branch_name;
+
 
 -- OTAZKA 2
 -- Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
 
-SELECT *, round(t2.avg_payroll/t1.avg_price, 2) AS kupyschopnost
+SELECT *, round(t2.avg_payroll/t1.avg_price, 2) AS purchasing_power
 FROM (
     SELECT price_year, price_cat_name, avg(avg_price) AS avg_price
         FROM t_kristina_majerova_project_sql_primary_final
@@ -84,7 +81,7 @@ FROM (
     GROUP BY price_year, price_cat_name
     ) t1
 JOIN (
-    SELECT price_year , round(avg(avg_payroll),2) AS avg_payroll
+    SELECT price_year, round(avg(avg_payroll),2) AS avg_payroll
         FROM t_kristina_majerova_project_sql_primary_final
     GROUP BY price_year
     ) t2
@@ -92,7 +89,7 @@ ON t1.price_year=t2.price_year;
 
 -- Var 2  - riešenie po jednotlivých odvetviach 
 
-SELECT *, round(t2.avg_payroll/t1.avg_price, 2) AS kupyschopnost
+SELECT *, round(t2.avg_payroll/t1.avg_price, 2) AS purchasing_power
 FROM (
     SELECT price_year, price_cat_name, avg(avg_price) AS avg_price
         FROM t_kristina_majerova_project_sql_primary_final
@@ -105,7 +102,7 @@ FROM (
     GROUP BY price_year, price_cat_name
     ) t1
 JOIN (
-    SELECT price_year, industry_branch_name , round(avg(avg_payroll),2) AS avg_payroll
+    SELECT price_year, industry_branch_name, round(avg(avg_payroll),2) AS avg_payroll
         FROM t_kristina_majerova_project_sql_primary_final
     GROUP BY price_year, industry_branch_name
     ) t2
@@ -117,30 +114,31 @@ ON t1.price_year=t2.price_year;
  
 -- riešenie s použitím 
  
- SELECT avg(rast_avg_price), price_cat_name
-from(
+ SELECT avg(growth_avg_price), price_cat_name
+FROM(
     SELECT *, 
         lag(avg_price,1) OVER (PARTITION BY price_cat_name ORDER BY price_year) AS avg_price_lag,
-        round(avg_price/lag(avg_price,1) OVER (PARTITION BY price_cat_name ORDER BY price_year)*100-100,2) AS rast_avg_price
-    from(
+        round(avg_price/lag(avg_price,1) OVER (PARTITION BY price_cat_name ORDER BY price_year)*100-100,2) AS growth_avg_price
+    FROM(
         SELECT price_year, avg(avg_price) AS avg_price, price_cat_name 
         FROM t_kristina_majerova_project_sql_primary_final
-        GROUP BY price_cat_name , price_year
+        GROUP BY price_cat_name, price_year
         )base
     )base2
  GROUP BY price_cat_name
- ORDER BY (avg(rast_avg_price));
+ ORDER BY (avg(growth_avg_price));
  
 -- OTAZKA 4
 -- Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)? 
  
--- relativny % rastu za predpokladu ze v oboch obdobiach bol rast
+-- riešenie cez odčítania hodnôt percentuálneho rastu miezd od rastu cien v jednotlivých rokoch 
  
-SELECT *, round((price.rast_avg_price-payroll.rast_avg_payroll)/payroll.rast_avg_payroll*100,2) AS perc_pomer_rastu
+
+SELECT payroll.payroll_year, price.growth_avg_price, payroll.growth_avg_payroll, price.growth_avg_price-payroll.growth_avg_payroll AS perc_growth_diff
 FROM 
     (SELECT *, 
-        lag(avg_payroll,1) OVER (ORDER BY payroll_year) AS avg_payroll_lag,
-        round(avg_payroll/lag(avg_payroll,1) OVER (ORDER BY payroll_year)*100-100,2) AS rast_avg_payroll
+        lag(avg_payroll,1) OVER (ORDER BY payroll_year) AS growth_payroll_lag,
+        round(avg_payroll/lag(avg_payroll,1) OVER (ORDER BY payroll_year)*100-100,2) AS growth_avg_payroll
         FROM (
             SELECT payroll_year, round(avg(avg_payroll),2)AS avg_payroll
             FROM t_kristina_majerova_project_sql_primary_final
@@ -150,7 +148,7 @@ FROM
 LEFT join
     (SELECT *, 
         lag(avg_price,1) OVER (ORDER BY price_year) AS avg_price_lag,
-        round(avg_price/lag(avg_price,1) OVER (ORDER BY price_year)*100-100,2) AS rast_avg_price
+        round(avg_price/lag(avg_price,1) OVER (ORDER BY price_year)*100-100,2) AS growth_avg_price
         from( 
             SELECT price_year, round(avg(avg_price),2) AS avg_price
             FROM t_kristina_majerova_project_sql_primary_final
@@ -158,19 +156,19 @@ LEFT join
             )t2
         )price 
 ON payroll.payroll_year=price.price_year
-WHERE round((price.rast_avg_price-payroll.rast_avg_payroll)/payroll.rast_avg_payroll*100,2) > 10
-;
+WHERE price.growth_avg_price-payroll.growth_avg_payroll > 10;
 
+-- zobrazenie rozdielov v jednotlivých rokoch zostupne: 
 
--- Otázka 5
--- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
-
-CREATE OR replaceVIEW v_kristina_majerova_project_sql_primary_final2 as
-SELECT payroll.payroll_year, payroll.rast_avg_payroll, price.rast_avg_price, tkmpssf.rast_gdp_per_capita
+SELECT 
+    payroll.payroll_year, 
+    price.growth_avg_price, 
+    payroll.growth_avg_payroll, 
+    price.growth_avg_price-payroll.growth_avg_payroll AS perc_growth_diff
 FROM 
     (SELECT *, 
-        lag(avg_payroll,1) OVER (ORDER BY payroll_year) AS avg_payroll_lag,
-        round(avg_payroll/lag(avg_payroll,1) OVER (ORDER BY payroll_year)*100-100,2) AS rast_avg_payroll
+        lag(avg_payroll,1) OVER (ORDER BY payroll_year) AS growth_payroll_lag,
+        round(avg_payroll/lag(avg_payroll,1) OVER (ORDER BY payroll_year)*100-100,2) AS growth_avg_payroll
         FROM (
             SELECT payroll_year, round(avg(avg_payroll),2)AS avg_payroll
             FROM t_kristina_majerova_project_sql_primary_final
@@ -180,8 +178,37 @@ FROM
 LEFT join
     (SELECT *, 
         lag(avg_price,1) OVER (ORDER BY price_year) AS avg_price_lag,
-        round(avg_price/lag(avg_price,1) OVER (ORDER BY price_year)*100-100,2) AS rast_avg_price
+        round(avg_price/lag(avg_price,1) OVER (ORDER BY price_year)*100-100,2) AS growth_avg_price
         from( 
+            SELECT price_year, round(avg(avg_price),2) AS avg_price
+            FROM t_kristina_majerova_project_sql_primary_final
+            GROUP BY price_year
+            )t2
+        )price 
+ON payroll.payroll_year=price.price_year
+ORDER BY perc_growth_diff DESC; 
+
+
+-- Otázka 5
+-- Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo násdujícím roce výraznějším růstem?
+
+CREATE OR REPLACE VIEW v_kristina_majerova_project_sql_primary_final2 AS
+SELECT payroll.payroll_year, payroll.growth_avg_payroll, price.growth_avg_price, tkmpssf.growth_gdp_per_capita
+FROM 
+    (SELECT *, 
+        lag(avg_payroll,1) OVER (ORDER BY payroll_year) AS avg_payroll_lag,
+        round(avg_payroll/lag(avg_payroll,1) OVER (ORDER BY payroll_year)*100-100,2) AS growth_avg_payroll
+        FROM (
+            SELECT payroll_year, round(avg(avg_payroll),2)AS avg_payroll
+            FROM t_kristina_majerova_project_sql_primary_final
+            GROUP BY payroll_year
+            ) t1
+        )payroll
+LEFT JOIN 
+    (SELECT *, 
+        lag(avg_price,1) OVER (ORDER BY price_year) AS avg_price_lag,
+        round(avg_price/lag(avg_price,1) OVER (ORDER BY price_year)*100-100,2) AS growth_avg_price
+        FROM( 
             SELECT price_year, round(avg(avg_price),2) AS avg_price
             FROM t_kristina_majerova_project_sql_primary_final
             GROUP BY price_year
@@ -189,22 +216,25 @@ LEFT join
         )price 
 ON payroll.payroll_year=price.price_year
 LEFT JOIN t_kristina_majerova_project_sql_secondary_final tkmpssf 
-ON payroll.payroll_year=tkmpssf.YEAR;
+ON payroll.payroll_year=tkmpssf.`year`;
 
 -- porovnanie rastu HDP a rastu miezd, cien v aktuálnom roku : 
 
 SELECT *, 
-rast_avg_price/rast_gdp_per_capita, 
-rast_avg_payroll/rast_gdp_per_capita
+growth_avg_price/growth_gdp_per_capita, 
+growth_avg_payroll/growth_gdp_per_capita
 FROM v_kristina_majerova_project_sql_primary_final2
-ORDER BY rast_gdp_per_capita DESC ;
+ORDER BY growth_gdp_per_capita DESC ;
 
 -- porovnanie rastu HDP a rastu miezd, cien v nasledujúcom roku :
 
-SELECT payroll_year, rast_avg_price, rast_avg_payroll,
-lag(rast_gdp_per_capita)OVER(ORDER BY payroll_year) AS lag_rast_gdp,
-rast_avg_price/lag(rast_gdp_per_capita)OVER(ORDER BY payroll_year), 
-rast_avg_payroll/lag(rast_gdp_per_capita)OVER(ORDER BY payroll_year)
+SELECT 
+    payroll_year, 
+    growth_avg_price, 
+    growth_avg_payroll,
+    lag(growth_gdp_per_capita)OVER(ORDER BY payroll_year) AS lag_growth_gdp,
+    growth_avg_price/lag(growth_gdp_per_capita)OVER(ORDER BY payroll_year), 
+    growth_avg_payroll/lag(growth_gdp_per_capita)OVER(ORDER BY payroll_year)
 FROM v_kristina_majerova_project_sql_primary_final2
-ORDER BY lag(rast_gdp_per_capita)OVER(ORDER BY payroll_year) DESC ;
+ORDER BY lag(growth_gdp_per_capita)OVER(ORDER BY payroll_year) DESC ;
 
